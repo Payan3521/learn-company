f1cdfc18511eb7ef827f028b52da1932961c87ed
payan3521 <pablobedoya3521@gmail.com>
2025-09-11 07:19:03 -0500

    initial commit

---
+++ src/main/java/com/desarrollox/learncompany/LearncompanyApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollox.learncompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearncompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearncompanyApplication.class, args);
+	}
+
+}
