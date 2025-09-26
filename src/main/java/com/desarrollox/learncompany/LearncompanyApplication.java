7f90d94f833ecd4edc3834466e826ab8da887d5a
Agudelo3075 <sebasagudelo1012@gmail.com>
2025-09-26 17:34:27 -0500

    Agregando variables de entorno

--- src/main/java/com/desarrollox/learncompany/LearncompanyApplication.java
+++ src/main/java/com/desarrollox/learncompany/LearncompanyApplication.java
@@ -3,10 +3,24 @@ package com.desarrollox.learncompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LearncompanyApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+			.ignoreIfMissing()
+			.load();
+
+		if(dotenv!=null){
+			dotenv.entries().forEach(entry -> {
+				if(System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey())==null){
+					System.setProperty(entry.getKey(), entry.getValue());
+				}
+			});
+		}
 		SpringApplication.run(LearncompanyApplication.class, args);
 	}
 
