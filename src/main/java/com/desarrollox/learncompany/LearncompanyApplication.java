e7108d6feb834115259231b48fe647af953e7ea7
payan3521 <pablobedoya3521@gmail.com>
2025-10-05 23:30:57 -0500

    organizado variables de entorno

--- src/main/java/com/desarrollox/learncompany/LearncompanyApplication.java
+++ src/main/java/com/desarrollox/learncompany/LearncompanyApplication.java
@@ -8,7 +8,18 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class LearncompanyApplication {
 
 	public static void main(String[] args) {
+	
+		Dotenv dotenv = Dotenv.configure()
+		.ignoreIfMissing()
+		.load();
 
+		if(dotenv!=null){
+			dotenv.entries().forEach(entry -> {
+				if(System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey())==null){
+					System.setProperty(entry.getKey(), entry.getValue());
+				}
+			});
+		}
 
 		SpringApplication.run(LearncompanyApplication.class, args);
 	}
