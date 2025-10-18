06123a2369e9e294d299bf41ec17e60688e8ea18
payan3521 <pablobedoya3521@gmail.com>
2025-10-18 15:01:38 -0500

    EmployeeBadge

--- src/main/java/com/desarrollox/learncompany/web/controller/EmployeeBadgeController.java
+++ src/main/java/com/desarrollox/learncompany/web/controller/EmployeeBadgeController.java
@@ -20,6 +20,7 @@ import com.desarrollox.learncompany.web.webMapper.EmployeeBadgeWebMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -37,29 +38,55 @@ public class EmployeeBadgeController {
     private final EmployeeBadgeWebMapper employeeBadgeWebMapper;
 
     @Operation(
-        summary = "Asignar una insignia a un empleado",
-        description = "Permite asignar una insignia existente a un empleado específico.",
+        summary = "Asignar nuevo badge a un empleado",
+        description = "Permite asignar un badge a un empleado del sistema.",
         requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
             required = true,
             content = @Content(
                 mediaType = "application/json",
                 examples = @ExampleObject(
-                    name = "Ejemplo de asignación de insignia",
+                    name = "Ejemplo de solicitud",
                     value = """
-                    {
-                      "employeeId": 3,
-                      "badgeId": 2,
-                      "dateAssigned": "2025-10-15"
-                    }
-                    """
+                        {
+                            "employeeId":1,
+                            "badgeId":1
+                        }
+                        """
                 )
+
             )
         ),
         responses = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Insignia asignada correctamente"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Datos inválidos o incompletos", content = @Content),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Empleado o insignia no encontrados", content = @Content),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "201", 
+                description = "insignia asignada correctamente",
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                        name = "Ejemplo de respuesta exitosa",
+                        value = """
+                            {
+                                "success": true,
+                                "message": "insignia asignada correctamente",
+                                "data": {
+                                    "id": 1,
+                                    "employeeId": 1,
+                                    "badgeId": 1,
+                                    "dateEarned": "2025-10-18T14:55:39.300725188"
+                                },
+                                "timestamp": "2025-10-18T14:55:39.384953526"
+                            } 
+                            """
+                    )
+                )
+
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Datos invalidos"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = ""),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = ""),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Objeto no encontrado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "409", description = "Badge ya asignado a empleado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Error interno en el servidor")
         }
     )
     @PostMapping
@@ -71,12 +98,43 @@ public class EmployeeBadgeController {
     }
 
     @Operation(
-        summary = "Eliminar una insignia asignada a un empleado",
-        description = "Elimina la relación entre un empleado y una insignia asignada.",
+        summary = "Eliminar una asignación de badge",
+        description = "Elimina el registro de una asignacion de badge según su ID.",
+        parameters = {
+            @Parameter(
+                name = "id",
+                description = "Identificados del EmployeeBadge a eliminar",
+                required = true,
+                example = "1"
+            )
+        },
         responses = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Insignia eliminada correctamente del empleado"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Relación de insignia y empleado no encontrada", content = @Content),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "200",
+                description = "Insignia eliminada a empleado correctamente",
+                content = @Content(
+                    examples = @ExampleObject(
+                        name = "Ejemplo de respuesta exitosa",
+                        value = """
+                            {
+                                "success": true,
+                                "message": "Insignia eliminada a empleado correctamente",
+                                "data": {
+                                    "id": 1,
+                                    "employeeId": 1,
+                                    "badgeId": 1,
+                                    "dateEarned": "2025-10-18T14:55:39"
+                                },
+                                "timestamp": "2025-10-18T15:00:56.258509608"
+                            }
+                            """
+                    )
+                )
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = ""),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = ""),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Objeto no encontrado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Error interno en el servidor")
         }
     )
     @DeleteMapping("/{id}")
@@ -87,29 +145,99 @@ public class EmployeeBadgeController {
     }
 
     @Operation(
-        summary = "Obtener una insignia asignada por ID",
-        description = "Obtiene la información de una relación específica entre empleado e insignia.",
+        summary = "Obtener una asignación de badge por ID",
+        description = "Busca y devuelve la información de una asignacion de badge según su ID.",
+        parameters = {
+            @Parameter(
+                name = "id",
+                description = "Identificados del EmployeeBadge a buscar",
+                required = true,
+                example = "1"
+            )
+        },
         responses = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Relación encontrada correctamente"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Relación no encontrada", content = @Content)
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "200",
+                description = "insignia de empleado obtenido correctamente",
+                content = @Content(
+                    examples = @ExampleObject(
+                        name = "Ejemplo de respuesta exitosa",
+                        value = """
+                            {
+                                "success": true,
+                                "message": "insignia de empleado obtenido correctamente",
+                                "data": {
+                                    "id": 1,
+                                    "employeeId": 1,
+                                    "badgeId": 1,
+                                    "dateEarned": "2025-10-18T14:55:39"
+                                },
+                                "timestamp": "2025-10-18T14:57:55.236537606"
+                            }  
+                            """
+                    )
+                )
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = ""),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = ""),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Objeto no encontrado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Error interno en el servidor")
         }
     )
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<EmployeeBadgeResponse>> findById(@PathVariable Long id){
         EmployeeBadge employeeBadge = employeeBadgeService.findById(id).get();
         EmployeeBadgeResponse employeeBadgeResponse = employeeBadgeWebMapper.domainToResponse(employeeBadge);
-        return ResponseEntity.ok(ApiResponse.success("isignia de empleado obtenido correctamente", employeeBadgeResponse));
+        return ResponseEntity.ok(ApiResponse.success("insignia de empleado obtenido correctamente", employeeBadgeResponse));
 
     }
 
     @Operation(
-        summary = "Obtener todas las insignias asignadas a empleados",
-        description = "Devuelve una lista con todas las insignias asignadas a empleados registrados.",
+        summary = "Obtener todas las asignaciones de badge",
+        description = "Devuelve la lista completa de las asignaciones de badge.",
         responses = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Lista de insignias de empleados obtenida correctamente"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "No hay insignias asignadas a empleados", content = @Content)
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "200",
+                description = "insignias encontradas",
+                content = @Content(
+                    examples = @ExampleObject(
+                        name = "Ejemplo de respuesta exitosa",
+                        value = """
+                            {
+                                "success": true,
+                                "message": "insignias encontradas",
+                                "data": [
+                                    {
+                                        "id": 1,
+                                        "employeeId": 1,
+                                        "badgeId": 1,
+                                        "dateEarned": "2025-10-18T14:55:39"
+                                    },
+                                    {
+                                        "id": 2,
+                                        "employeeId": 1,
+                                        "badgeId": 2,
+                                        "dateEarned": "2025-10-18T14:55:39"
+                                    },
+                                    {
+                                        "id": 3,
+                                        "employeeId": 1,
+                                        "badgeId": 3,
+                                        "dateEarned": "2025-10-18T14:55:39"
+                                    }
+                                ],
+                                "timestamp": "2025-10-18T14:59:16.225922105"
+                            }   
+                            """
+                    )
+                )
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "Lista vacia"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = ""),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = ""),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Error interno en el servidor")
         }
-    )
+    ) 
     @GetMapping
     public ResponseEntity<ApiResponse<List<EmployeeBadgeResponse>>> findAll(){
         List<EmployeeBadge> employeeBadges = employeeBadgeService.findAll();
